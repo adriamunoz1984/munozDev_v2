f5670a75f92cec504d698154deb9fcb1bc1ba1a9
adrian <am120284@gmail.com>
2018-07-08 15:49:25 -0700

    file load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { EducationComponent } from './education/education.component';
+import { DemosComponent } from './demos/demos.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SkillsComponent,
+    ExperiencesComponent,
+    EducationComponent,
+    DemosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
